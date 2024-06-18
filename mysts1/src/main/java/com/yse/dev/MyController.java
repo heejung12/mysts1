9b28a94da3588beeae53ea0f6274870c218efbd1
heejung12 <redphj369@gmail.com>
2024-06-18 10:12:22 +0900

    commit

--- mysts1/src/main/java/com/yse/dev/MyController.java
+++ mysts1/src/main/java/com/yse/dev/MyController.java
@@ -3,18 +3,17 @@ package com.yse.dev;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class MyController {
 	@GetMapping("/")
-	@ResponseBody
 	public String myRoot() {
 		return "hello my Site!";
 	}
 	
 	@GetMapping("/greeting")
-	@ResponseBody
 	public String mygreet() {
 		return "나의 사이트에 오신걸 환영합니다!";
 	}
